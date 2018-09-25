6e86c52cdf3e6115be4fb45f127fbd21141d3417
LongClipeus <longlevan010@gmail.com>
2018-09-25 19:44:17 +0700

    learn sync and async

--- 9. buffer/read-file.js
+++ 9. buffer/read-file.js
@@ -1,7 +1,15 @@
 var fs = require("fs");
 
+// read file sync 
 var content = fs.readFileSync(__dirname + "/ds.txt");
-
 console.log(content);
-console.log(content.toString());
+console.log('call sync');
+
+// read file async 
+fs.readFile(__dirname + "/ds.txt", "utf8", function (err, data) {  
+    console.log('call async');      
+    var buffer = new Buffer(data);
+    console.log(buffer);
+});
+console.log("Done");
 
