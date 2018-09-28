59fe3f69e097b4c9c6418341f814d39f18ea2701
LongClipeus <longlevan010@gmail.com>
2018-09-28 19:20:41 +0700

    Doc tham so truyen vao tu console

--- hello-world.js
+++ hello-world.js
@@ -4,3 +4,15 @@ var b = 2;
 console.log('Hello world');
 
 console.log(a + b);
+
+/* --------- Doc tham so truyen vao tu console ---------------- */
+var name = process.argv[2];
+console.log('Hello ' + name);		// lay ten tu
+
+// in ket qua len man hinh console
+process.stdout.write(`Welcome to nodejs, ${name}\n`);
+
+// Doc cac tham so da truyen
+process.argv.forEach(function(val, index, array) {
+   console.log(index + ': ' + val);
+});
